a8699964f3884caa0867fbfda2a164f7e7eb5667
Timo Moilanen <t3moti00@students.oamk.fi>
2026-04-06 12:06:50 +0300

    main merge to timo + db conflict override  selvitys

--- Database/Database.tsx
+++ Database/Database.tsx
@@ -1,5 +1,5 @@
 import * as SQLite from 'expo-sqlite';
-import { DbProps, UserData, UserWeight } from '../types/database';
+import { DbProps, Exercise, UserData, UserWeight } from '../types/database';
 
 export function Database({db, setDb, setUserData, setUserWeight}: DbProps)
 {
@@ -75,4 +75,19 @@ export const AddProfile = async (etuNimi: string, sukuNimi: string, ikä: string
   if (!database) return;
   console.log("dbpurge")
   await database.runAsync('DELETE FROM UserData')
+}
+export const loadGymData = async (
+  setgymExerList: React.Dispatch<React.SetStateAction<Exercise[]>>) => {
+  console.log("ennen")
+  const database = await SQLite.openDatabaseAsync('JogAppDb3dev.db');
+  if(!database) return
+  const sql = database.sql
+  
+  const tableData= await sql<Exercise>`SELECT * FROM GymData ORDER BY GymDataID DESC`;
+  console.log("tässä on " +tableData[1].Rest_Time_Minutes)
+  setgymExerList(tableData)
+};
+export const AddExercise = async (lepo: string, toisto: string, paino: string, liike: string, sarja: string, db: SQLite.SQLiteDatabase | null) => {
+  if (!db) return;
+  const execData = await db.runAsync('INSERT INTO GymData (UserID, Rest_Time_Minutes, Repetitions, Weight_Kg, Exercise_Type, Set_Amount) VALUES (1,?,?,?,?,?)', lepo, toisto, paino, liike, sarja)
 }
