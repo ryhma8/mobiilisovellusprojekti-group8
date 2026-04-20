6c000cebb76c6addbe0bc76cad7be6e883c5a04c
Timo Moilanen <t3moti00@students.oamk.fi>
2026-04-20 12:35:13 +0300

    osien mergaus

---
+++ components/TreeniCard.tsx
@@ -0,0 +1,62 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Exercise, Training, UserData, UserWeight } from '../types/database';
+//import { Database } from '../Database/Database';
+import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
+import { useSQLiteContext } from 'expo-sqlite';
+
+
+interface TreeniProps {
+    item: Training,
+    TrainDataID: number;
+    OnPress?: () => void;
+    SetTrainToDay?: (id: string) => void;
+    toggleSelect?: (id: string) => void;
+    selected?: boolean;
+}
+
+
+
+export default function TreeniCard({ item, SetTrainToDay,selected, OnPress }: TreeniProps) {
+
+
+    const db = useSQLiteContext(); //ladataan database konstekstista
+
+
+    return (
+        <View>
+            <TouchableOpacity
+                onPress={() => {SetTrainToDay?.(item.TrainDataID);
+                    OnPress?.();
+                }}
+                style={[
+                    styles.liike,
+                    selected && styles.selectedLiike]}
+            >
+                <Text style={styles.liikeText}>{item.TrainName}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+    liike: {
+        backgroundColor: '#f6f6f6ff',
+        margin: 10,
+        padding: 5,
+        height: 60,
+        width: 250,
+        borderRadius: 4,
+    },
+    selectedLiike: {
+        backgroundColor: '#fc8bd2ff',
+    },
+    liikeText: {
+        color: '#000',
+        fontSize:20,
+        
+    },
+    treeniCard: {
+        alignItems:'center'
+    }
+});
--- components/TreeniListaModal.tsx
+++ components/TreeniListaModal.tsx
@@ -1,49 +1,133 @@
 import React, { useEffect, useState } from 'react';
-import { Modal, StyleSheet, Text, Pressable, View, Dimensions, TextInput, Button } from 'react-native';
-import { TreeniListaModalProps, TreeniModalProps } from '../types/ModalProps';
-import { horizontalScale } from '../mathFunctions/FonttiSkaalaaja';
-import { LiikeModal } from './LiikeModal';
-import LiikeCard from './LiikeCard';
-import { Exercise } from '../types/database';
-import { loadGymData } from '../Database/Database';
+import { Modal, StyleSheet, Text, Pressable, View, Dimensions, FlatList, ScrollView } from 'react-native';
+import { TreeniListaModalProps } from '../types/ModalProps';
+import { Exercise, Training } from '../types/database';
+import { deleteTraining, loadGymData, loadTrainData } from '../Database/Database';
+
+import TreeniCard from './TreeniCard';
+import ExerciseCard from './ExerciseCard';
 import { TreeniModal } from './TreeniModal';
 
-const { width, height } = Dimensions.get("window");
 
 export function TreeniListaModal({ modalVisibleTreeniLista, setModalVisibleTreeniLista, db }: TreeniListaModalProps) {
     const [modalVisibleTreeni, setModalVisibleTreeni] = useState(false);
+    const [modalTraining, setModalTraining] = useState(false);
+    const [gymTrainList, setGymTrainList] = useState<Training[]>([]);
+    const [gymExerList, setGymExerList] = useState<Exercise[]>([]);
+    const [selectedTraining, setSelectedTraining] = useState<Training | null>(null);
+    const [toBeDeleted, setToBeDeleted]= useState<number>(0)
+    const [refresh, setRefresh] = useState(false)
 
-    const numerotest = 1
+    const exerciseMap = Object.fromEntries(
+        gymExerList.map(e => [e.GymDataID, e])
+    );
 
+    useEffect(() => {
+        if(refresh){
+            setRefresh(false)
+        }
+        console.log("refresh")
+        loadGymData(setGymExerList, db);
+        loadTrainData(setGymTrainList, db);
+        console.log("gymexlist: ", gymTrainList)
+    }, [refresh,modalVisibleTreeni]);
 
+    const exerciseIds = selectedTraining
+        ? [
+            selectedTraining.Exec1,
+            selectedTraining.Exec2,
+            selectedTraining.Exec3,
+            selectedTraining.Exec4,
+            selectedTraining.Exec5,
+            selectedTraining.Exec6,
+            selectedTraining.Exec7,
+            selectedTraining.Exec8,
+            selectedTraining.Exec9,
+            selectedTraining.Exec10,
+        ].filter(Boolean)
+        : [];
 
+        function deleteTrain(){
+            console.log("inside call")
+            deleteTraining(toBeDeleted,db)
+        }
     return (
         <View>
-            <Pressable
-
-                onPress={() => setModalVisibleTreeniLista(true)}>
+            <Pressable onPress={() => setModalVisibleTreeniLista(true)}>
                 <Text style={styles.modalNappi}>Treenit</Text>
             </Pressable>
 
             <Modal
                 animationType="slide"
                 visible={modalVisibleTreeniLista}>
-
                 <View style={styles.ohjelmaModal}>
 
                     <Text style={styles.otsikko}>Treenit</Text>
+                    <FlatList
+                        data={gymTrainList}
+                        keyExtractor={(item) => item.TrainDataID.toString()}
+                        renderItem={({ item }) => (
+                            <TreeniCard
+                                item={item}
+                                TrainDataID={item.TrainDataID}
+                                OnPress={() => {
+                                    setSelectedTraining(item);
+                                    setModalTraining(true);
+                                    setToBeDeleted(item.TrainDataID);
+                                }}
+                            />
+                        )}
+                    />
+
+                    <Modal
+                        animationType="slide"
+                        visible={modalTraining}>
+                        <View style={styles.ohjelmaModal}>
+
+                            <Text style={styles.otsikko}>
+                                {selectedTraining?.TrainName}
+
+                            </Text>
+                            <ScrollView>
+                                {exerciseIds.map((id, index) => {
+                                    const exercise = exerciseMap[Number(id)];
+                                    if (!exercise) return null;
+
+                                    return (
+                                        <ExerciseCard key={index} exercise={exercise} />
+                                    );
+                                })}
+                            </ScrollView>
+                            <View style={styles.modalNappiRivi}>
+                                <Pressable onPress={() => [setModalTraining(false)]}>
+                                    <Text style={styles.modalNappi}>Sulje</Text>
+                                </Pressable>
+                                <Pressable onPress={() => [console.log("selected: ",toBeDeleted),
+                                    deleteTrain(),
+                                    setModalTraining(false),
+                                    setRefresh(true)
+                                ]}>
+                                    <Text style={styles.modalNappi}>Poista </Text>
+                                </Pressable>
+                            </View>
+
+                        </View>
+                    </Modal>
 
                     <View style={styles.modalNappiRivi}>
-                        <Pressable
-                            onPress={() => setModalVisibleTreeniLista(false)}>
+
+                        <Pressable onPress={() => setModalVisibleTreeniLista(false)}>
                             <Text style={styles.modalNappi}>Sulje</Text>
                         </Pressable>
+                        
+
 
                         <TreeniModal
                             modalVisibleTreeni={modalVisibleTreeni}
                             setModalVisibleTreeni={setModalVisibleTreeni}
                             db={db}
-                        ></TreeniModal>
+                        />
+
                     </View>
 
                 </View>
@@ -89,5 +173,10 @@ const styles = StyleSheet.create({
     ohjelmaModal: {
         backgroundColor: '#9F6BFB',
         flex: 1
+    },
+    exerciseText: {
+        paddingLeft: 10,
+        paddingBottom: 10,
+        color: '#fff'
     }
 });
